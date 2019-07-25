a22e35f264283544091381d284bb3e8b5c4f70ea
Ujjwal <ujjsilwal@live.com>
2019-07-25 17:07:41 +1000

    Time added

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 
 const exphbs = require('express-handlebars');
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+mongoose.connect(keys.MongoURI)
+.then(() => {
+console.log('Remote MongoDB is connected...');
+});
 const app = express();
 // setup template engine
 app.engine('handlebars', exphbs({
@@ -12,6 +18,29 @@ app.set('view engine', 'handlebars');
 //setup express static public folder for css and js and images
 
 app.use('/',express.static('public'));
+//setup body parser to encode URL
+app.use(bodyParser.urlencoded({
+    extented: false
+}));
+app.use(bodyParser.json());
+//create collectionnwith mongoose
+const Schema = mongoose.Schema;
+const Message = mongoose.model('message',new Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    message: {
+        type: String
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+
+}));
 const port = process.env.PORT || 3000;
 
 app.get('/home',(req, res) => {
@@ -25,6 +54,29 @@ app.get('/about',(req, res) => {
 app.get('/contact',(req, res) => {
     res.render('contact');
 });
+app.post('/getMessage', (req,res) => {
+    const newMessage ={
+        name: req.body.name,
+        email:req.body.email,
+        message:req.body.message
+    }
+    new Message(newMessage).save()
+    .then(() => {
+      res.render('inbox');
+    });
+});
+
+app.get('/displayMessage',(req,res) =>{
+   Message.find({}, (err,messages) => {
+    if(err){
+        console.log(err);
+    }else{
+        res.render('displayMessage' , {
+            messages:messages
+        })
+    }
+})
+})
 //This is portfolio route handler
 app.get('/portfolio',(req, res) => {
     res.render('portfolio');
